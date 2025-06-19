53c6432c9f86dc458e04a5a87b914119b2cb04c0
NaDario Seays <nadario.seays@gmail.com>
2025-06-19 15:59:16 +0000

    Adds back default.nix

---
+++ Config/default.nix
@@ -0,0 +1,15 @@
+{  config, pkgs, ... }
+{
+  environment.systemPackages = with pkgs; [
+    # General utilities
+    vim
+    git
+    curl
+    wget
+    # Development tools
+    gcc
+    make
+    python3
+    # Add more packages as needed
+  ];
+}
