6a976e2c2e2c75ff4a7f8a7b8a48bb23799a0949
NaDario Seays <nadario.seays@gmail.com>
2025-06-19 19:22:22 +0000

    w

--- Config/Modules/networking.nix
+++ Config/Modules/networking.nix
@@ -1,6 +1,6 @@
 {
  # Enable networking
-  # networking.networkmanager.enable = false;
+  networking.networkmanager.enable = ;
   
   networking.hostName = "framework13"; # Define your hostname.
   # networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
