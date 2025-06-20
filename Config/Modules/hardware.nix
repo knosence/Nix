f8d870c0995a8ae919bb9b7e88ca9f5140294885
NaDario Seays <nadario.seays@gmail.com>
2025-06-20 18:06:21 +0000

    AJ

--- Config/Modules/hardware.nix
+++ Config/Modules/hardware.nix
@@ -28,7 +28,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/46BB-1DC7";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0022" "dmask=0022" ]
     };
 
   swapDevices = [ ];
