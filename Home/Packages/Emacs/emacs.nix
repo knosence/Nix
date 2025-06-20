889e49c846ff7299cd6c48b347e4ca4d6545a761
NaDario Seays <nadario.seays@gmail.com>
2025-06-20 00:34:11 +0000

    tt

--- Home/Packages/Emacs/emacs.nix
+++ Home/Packages/Emacs/emacs.nix
@@ -1,19 +1,12 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    doom-emacs    # pulls in the “doom” CLI and its core
-  ];
+
 
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;    # optional: your vanilla Emacs build
+    package = pkgs.emacs;    
   };
 
-  # Run `doom sync` automatically after switching
-  home.activation.doom-sync = lib.hm.doomEmacsActivation {
-    emacsPackage = pkgs.emacs;
-    doomPackage  = pkgs.doom-emacs;
-    doomDir      = config.home.homeDirectory + "/.doom.d";
-  };
+
 }
