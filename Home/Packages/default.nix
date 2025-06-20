8cc2cea35d04c52e858242ca2738e596a1f1b53c
NaDario Seays <nadario.seays@gmail.com>
2025-06-20 16:04:06 +0000

    AE

--- Home/Packages/default.nix
+++ Home/Packages/default.nix
@@ -4,13 +4,11 @@
   home.packages = with pkgs; ([  
     # User-specific packages
     brave
-    firefox
   ]);
   
   programs = {
     home-manager.enable = true;
-    firefox.enable = false; 
-  };
+ };
 
   
 
