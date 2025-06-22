32fddd867744fe365e59240eb39391e48abd4bc4
NaDario Seays <nadario.seays@gmail.com>
2025-06-22 05:23:33 +0000

    adds vm hardware

---
+++ Config/Hosts/virtual.nix
@@ -0,0 +1,41 @@
+# Host configuration for framework13
+#
+# This file defines host-specific settings, including user accounts, hardware, and packages.
+#
+# Usage: Imported by flake.nix under nixosConfigurations.framework13
+#
+# See also: ../Modules/ for hardware, package, and (optionally) user modules
+
+{ config, lib, pkgs, release, ... }:
+let
+    userName = "knosence";
+    userDesc = "NaDario Seays Sr.";
+    userGroups = [ "networkmanager" "wheel" "dialout" ];
+  
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./../Modules/boot.nix
+      ./../Modules/vmhardware.nix
+      ./../Modules/networking.nix
+      ./../Modules/security.nix
+      ./../Modules/services.nix
+      ./../Packages
+    ];
+    users.users.${userName} = {
+      isNormalUser = true;
+      description = userDesc;
+      extraGroups = userGroups;
+      packages = with pkgs; [ 
+
+      ]; # User-specific packages can go here
+    };
+
+  system.stateVersion = release; # Did you read the comment?
+
+   nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+}
