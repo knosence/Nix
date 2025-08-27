0fc610d68c0288481d930742115740cbb613ffbe
NaDario Seays <nadario.seays@gmail.com>
2025-08-27 01:39:26 +0000

    rm stupid git additions

--- Home/Packages/default.nix
+++ Home/Packages/default.nix
@@ -5,11 +5,8 @@
     # User-specific packages
     brave
     telegram-desktop
-<<<<<<< HEAD
     orca-slicer
     waveterm
-=======
->>>>>>> 130a998 (fixes error with emacs config files)
   ]);
 
   programs = {
